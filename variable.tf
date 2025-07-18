0762a94fd292f17897343ec44bd9c6269898b16b
shahkhan7 <80058324+shahkhan7@users.noreply.github.com>
2025-07-18 15:15:23 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 variable "tenant_id" {
   type        = string
   description = "billing tenant ID for Azure"
-  default     = "bd55c2ea-0ea2-43b0-844c-5f0f7514e38a"
+  default     = "xxxxxxxxxxxxxxxxxxxx"
 }
 variable "subscription_id" {
   type        = string
   description = "Your Azure Subscription ID"
-  default     = "15fb9914-0707-44fd-a9fa-96f741eac317" # Replace with your actual subscription ID
+  default     = "xxxxxxxxxxxxxxxx" # Replace with your actual subscription ID
 }
 variable "resource_group_name" {
   type        = string
